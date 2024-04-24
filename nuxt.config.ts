9dfe5d51d678be3ad6d05e42c292cf576c6240dc
Alfred White <alfred.white@hubnero.com>
2024-04-25 01:59:03 +0300

    commit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "nuxt-primevue"],
+  primevue: {
+    cssLayerOrder: "tailwind-base, primevue, tailwind-utilities",
+  },
+  css: ["primevue/resources/themes/aura-light-green/theme.css"]
+});
